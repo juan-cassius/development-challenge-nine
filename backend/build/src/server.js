e8e92437167dd4097a03c77366b19fc191741677
Juan Cassius <cassjuan@hotmail.com>
2023-11-19 19:38:46 -0300

    Backend: Fixed Model, deleted build

--- backend/build/src/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const App_1 = require("./App");
-const PORT = process.env.APP_PORT || 3001;
-new App_1.default().start(PORT);
-//# sourceMappingURL=server.js.map
