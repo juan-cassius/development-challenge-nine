2f4e160d40738f7d3667d969b3b0edbd588fc59a
Juan Cassius <cassjuan@hotmail.com>
2023-11-19 23:05:27 -0300

    Last details

--- frontend/src/components/DevCard/DevCard.tsx
+++ frontend/src/components/DevCard/DevCard.tsx
@@ -9,7 +9,8 @@ function DevCard() {
       <img src={devPicture} alt="dev" className='dev-info-card-body-picture' />
       <Typography variant='h4' className='dev-info-card-body-name'>Quem sou eu?</Typography>
       <Typography variant='body1' className='dev-info-card-body-text'>
-        Olá! Me chamo Juan Cassius, tenho 26 anos, sou músico e acadêmico de Análise e Desenvolvimento de Sistemas. Faço o curso de desenvolvimento fullstack da Trybe (Node/React) desde janeiro deste ano. Estou em constante aprendizado, não houve uma semana deste ano que não tenha aprendido um conceito, uma ferramenta ou uma metodologia relacionada a programação e meu aprimoramento pessoal. </Typography>
+        Olá! Me chamo Juan Cassius, tenho 26 anos, sou músico e acadêmico de Análise e Desenvolvimento de Sistemas. Faço o curso de desenvolvimento fullstack da Trybe (Node/React) desde janeiro deste ano. Estou em constante aprendizado, não houve uma semana deste ano que não tenha aprendido um conceito, uma ferramenta ou uma metodologia relacionada a programação e meu aprimoramento pessoal.
+      </Typography>
       <Typography variant='h4' className='dev-info-card-body-name'>Quer entrar em contato?</Typography>
       <div className='social-media'>
         <LinkedIn
